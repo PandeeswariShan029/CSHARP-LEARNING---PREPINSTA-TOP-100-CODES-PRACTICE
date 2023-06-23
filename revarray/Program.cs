1854c1e46c45b06652c3eef636bfb41dd60feec2
PandeeswariShan029 <127006903+PandeeswariShan029@users.noreply.github.com>
2023-06-23 17:53:10 +0530

    Add files via upload

---
+++ revarray/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class program
+{
+    static int[] revArray(int[] arr)
+    {
+        
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+
+            Console.WriteLine(arr[i]);
+
+
+
+        }
+
+        return arr;
+
+
+
+    }
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[size];
+
+
+        for (int j = 0; j < size; j++)
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int number))
+            {
+                arr[j] = number;
+            }
+            else
+            {
+                arr[j] = 0;
+            }
+
+        }
+
+        int[] get = revArray(arr);
+      
+        Console.ReadLine();
+
+    }
+}
