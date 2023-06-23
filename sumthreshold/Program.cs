0cfe3ba0c4d0fe832a8dc509aed45df00fa2df3d
PandeeswariShan029 <127006903+PandeeswariShan029@users.noreply.github.com>
2023-06-23 17:58:12 +0530

    Add files via upload

---
+++ sumthreshold/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class program
+{
+    static int CalculateSum(int[] arr, int threshold)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+
+        {
+            if ((sum + arr[i]) > threshold)
+            {
+                continue;
+                
+                
+            }
+            sum += arr[i];
+        }
+        return sum;
+    }
+    
+    static void Main()
+    {
+        int size = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[size];
+        
+        
+        for (int j = 0; j < size; j++)
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input,out int number))
+            {
+                arr[j] = number;
+            }
+            else
+            {
+                arr[j] = 0;
+            }
+
+        }
+        int thresh = int.Parse(Console.ReadLine());
+        int get = CalculateSum(arr, thresh);
+        Console.WriteLine(get);
+        Console.ReadLine();
+
+    }
+}
