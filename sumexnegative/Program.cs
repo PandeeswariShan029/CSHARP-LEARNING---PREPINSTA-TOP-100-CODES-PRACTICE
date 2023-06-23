68861ca4d77b4ae771ca9682557b80d13e0a70d1
PandeeswariShan029 <127006903+PandeeswariShan029@users.noreply.github.com>
2023-06-23 17:57:08 +0530

    Add files via upload

---
+++ sumexnegative/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int number))
+            {
+                arr[i] = number;
+            }
+            else
+            {
+                arr[i] = 0;
+            }
+
+        }
+        for (int j = 0; j < n; j++)
+        {
+
+            if (arr[j] >= 0)
+            {
+                sum += arr[j];
+            }
+        }
+
+            Console.WriteLine(sum);
+
+
+        }
+    }
