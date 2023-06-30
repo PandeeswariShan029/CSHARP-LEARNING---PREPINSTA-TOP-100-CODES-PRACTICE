7a25d25105f53d8264091c790c5d464e9d8f910c
Pandeeswari <127006903+PandeeswariShan029@users.noreply.github.com>
2023-06-30 11:56:24 +0530

    Add files via upload

---
+++ removeduplicates hash/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace removeduplicates
+{
+    internal class Program
+    {
+        static HashSet<int> removeduplicate(int[] arr,HashSet<int> hash)
+        {
+           
+           for(int i=0;i<arr.Length;i++)
+            {
+                hash.Add(arr[i]);   
+            }
+           return hash;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            for(int i=0;i<arr.Length;i++)
+            {
+                string input = Console.ReadLine();  
+                if(int.TryParse(input, out int num))
+                {
+                    arr[i] = num;
+                }
+                else
+                {
+                    arr[i] = 0;
+                }
+            }
+            HashSet<int> hash = new HashSet<int>();
+            HashSet<int> ha = removeduplicate(arr,hash);
+            Console.Write("Unique Elements: ");
+            foreach (int element in hash)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.ReadLine();
+             
+        }
+    }
+}
